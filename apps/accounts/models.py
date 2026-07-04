abe02db1e5d3faffc4332b014a50d6864ef92b83
priyanka <madakalapriyanka@gmail.com>
2026-07-04 20:11:26 +0530

    Initial project setup with Django, DRF, MySQL and custom user model

---
+++ apps/accounts/models.py
@@ -0,0 +1,29 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+
+class User(AbstractUser):
+
+    class Role(models.TextChoices):
+        ADMIN = "ADMIN", "Admin"
+        STUDENT = "STUDENT", "Student"
+        PARENT = "PARENT", "Parent"
+        FACULTY = "FACULTY", "Faculty"
+        DRIVER = "DRIVER", "Driver"
+
+    role = models.CharField(
+        max_length=20,
+        choices=Role.choices,
+        default=Role.STUDENT,
+    )
+
+    phone_number = models.CharField(max_length=15, blank=True)
+
+    profile_image = models.ImageField(
+        upload_to="profiles/",
+        blank=True,
+        null=True,
+    )
+
+    def __str__(self):
+        return self.username
